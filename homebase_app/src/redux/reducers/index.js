f6a11a69c9a5eef492cec1ab9533220f45afc6b4
MrT3313 <rturgeon13@gmail.com>
2019-05-26 22:01:06 -0500

    FE
    Redux Boilerplate

---
+++ homebase_app/src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+// REDUX
+    import { combineReducers } from "redux"
+
+// REDUCERS
+
+// -- *** -- START CODE -- *** -- //
+// -- *** -- START CODE -- *** -- //
+
+export const rootReducer = combineReducers({
+    
+})
