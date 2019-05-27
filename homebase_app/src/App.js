ff9dbe9e1725595aea99d5bed57265d895b6370c
MrT3313 <rturgeon13@gmail.com>
2019-05-27 12:43:20 -0500

    FE
    ReactRouting added

--- homebase_app/src/App.js
+++ homebase_app/src/App.js
@@ -1,15 +1,23 @@
 // REACT
-  import React from 'react';
+  import React, { Component } from 'react';
+  import { Route } from "react-router-dom";
+
 
 // COMPONENTS
   import Homepage from './views/Homepage'
+  import UserProfile from './views/UserProfile'
 
-function App() {
-  return (
-    <div className="App">
-      <Homepage /> 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <Route exact path='/'         component={Homepage} />
+        <Route exact path='/profile'  component={UserProfile} />
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ homebase_app/src/views/UserProfile.js
@@ -0,0 +1,37 @@
+// REACT 
+import React, { Component } from 'react';
+
+// REDUX 
+    // import { connect } from "react-redux"
+
+// COMPONENTS
+    import Header from '../components/Header'
+    import Footer from '../components/Footer'
+
+// -- *** -- START CODE -- *** -- //
+// -- *** -- START CODE -- *** -- //
+
+class UserProfile extends Component {
+
+
+    render() {
+        return (
+            <>
+                <Header />
+
+                <div>INSIDE USER PROFILE</div>
+                
+                <Footer />
+            </>
+        )
+    }
+}
+
+// MAP STATE TO PROPS
+
+// CONNECT
+
+
+
+
+export default UserProfile 
